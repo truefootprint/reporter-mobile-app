8783cc305e92683e6390fcd516ce2887e0d9e457
Chris Patuzzo <chris@patuzzo.co.uk>
2019-11-19 12:11:32 +0000

    Add a ScrollView example

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,11 +1,115 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { StyleSheet, View, ScrollView, Image, Text } from 'react-native';
+
 
 const Home = ({ navigation }) => (
-  <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-    <Text>Home Screen</Text>
-    <Button title="click me" onPress={() => navigation.navigate("Another")} />
-  </View>
+  <ScrollView stickyHeaderIndices={[0, 11, 22, 33, 44]}>
+    <View style={{ width: 400, height: 50, backgroundColor: 'red' }} />
+    <Text>Item 1</Text>
+    <Text>Item 2</Text>
+    <Text>Item 3</Text>
+    <Text>Item 4</Text>
+    <Text>Item 5</Text>
+    <Text>Item 6</Text>
+    <Text>Item 7</Text>
+    <Text>Item 8</Text>
+    <Text>Item 9</Text>
+    <Text>Item 10</Text>
+    <View style={{ width: 400, height: 50, backgroundColor: 'green' }} />
+    <Text>Item 11</Text>
+    <Text>Item 12</Text>
+    <Text>Item 13</Text>
+    <Text>Item 14</Text>
+    <Text>Item 15</Text>
+    <Text>Item 16</Text>
+    <Text>Item 17</Text>
+    <Text>Item 18</Text>
+    <Text>Item 19</Text>
+    <Text>Item 20</Text>
+    <View style={{ width: 400, height: 50, backgroundColor: 'blue' }} />
+    <Text>Item 21</Text>
+    <Text>Item 22</Text>
+    <Text>Item 23</Text>
+    <Text>Item 24</Text>
+    <Text>Item 25</Text>
+    <Text>Item 26</Text>
+    <Text>Item 27</Text>
+    <Text>Item 28</Text>
+    <Text>Item 29</Text>
+    <Text>Item 30</Text>
+    <View style={{ width: 400, height: 50, backgroundColor: 'orange' }} />
+    <Text>Item 31</Text>
+    <Text>Item 32</Text>
+    <Text>Item 33</Text>
+    <Text>Item 34</Text>
+    <Text>Item 35</Text>
+    <Text>Item 36</Text>
+    <Text>Item 37</Text>
+    <Text>Item 38</Text>
+    <Text>Item 39</Text>
+    <Text>Item 40</Text>
+    <View style={{ width: 400, height: 50, backgroundColor: 'purple' }} />
+    <Text>Item 41</Text>
+    <Text>Item 42</Text>
+    <Text>Item 43</Text>
+    <Text>Item 44</Text>
+    <Text>Item 45</Text>
+    <Text>Item 46</Text>
+    <Text>Item 47</Text>
+    <Text>Item 48</Text>
+    <Text>Item 49</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+    <Text>Item 50</Text>
+  </ScrollView>
 );
 
 export default Home;
